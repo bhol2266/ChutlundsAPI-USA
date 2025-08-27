3082716ec8fa176080ca5a2ce99e5ea4668bcb4b
ukdevelopers007 <113181134+ukdevelopers007@users.noreply.github.com>
2025-08-28 00:43:07 +0545

    asdfdsa

--- pages/api/getAntarvasnaCategories.js
+++ pages/api/getAntarvasnaCategories.js
@@ -8,13 +8,13 @@ export default async function handler(req, res) {
   }
 
   try {
-    const { page } = req.body;
+    const { url } = req.body;
 
     if (!page) {
       return res.status(400).json({ message: "Missing page number" });
     }
 
-    const response = await axios.get(`https://www.antarvasnaphotos2.com/page/${page}/`, {
+    const response = await axios.get(url, {
       headers: {
         "User-Agent":
           "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
